b6cf2ae0017638de081983456ab8fa24b36b2828
kowalczyk-mateusz <kowalczyk_mateusz@poczta.fm>
2021-04-07 10:47:21 +0200

    creating static props

--- components/layout.js
+++ components/layout.js
@@ -15,26 +15,26 @@ export default function Layout({children, home}){
                     name="description"
                     content="Learn how to build a personal website using next.js"
                 />
-                <meta 
-                    property="og:image"
-                    content={`https://og-image.vercel.app/${encodeURI(
-                        siteTitle
-                        )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-                        />
+                <meta
+          property="og:image"
+          content={`https://og-image.vercel.app/${encodeURI(
+            siteTitle
+          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+        />
                 <meta name="og:title" content={siteTitle} />
                 <meta name="twitter:card" content="summary_large_image"/>
             </Head>
             <header className="styles.header">
                 {home ? (
                     <>
-                        <Image 
-                            priority
-                            src="/images/profile.png"
-                            className={utilStyles.borderCircle}
-                            height={144}
-                            width={144}
-                            alt={name}
-                        />
+                       <Image
+              priority
+              src="/images/profile.jpg"
+              className={utilStyles.borderCircle}
+              height={144}
+              width={144}
+              alt={name}
+            />
                         <h1 className={utilStyles.heading2X1}>{name}</h1>
                     </>
                 ) : (
